ecf02edf3e401e31cd112f49f3d872f2357ae981
Joseph Feldmeyer <joseph.k.feldmeyer@gmail.com>
2020-06-22 13:23:25 +0900

    Finished Episode 1: Backgrounds & Tiles

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -7,3 +7,10 @@ export function loadImage(url) {
         image.src = url; 
     }); 
 } 
+
+export function loadLevel(name) {
+    return fetch(`/levels/${name}.json`) 
+    .then(r => r.json()); 
+} 
+
+
